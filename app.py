9278904357fc99ff43ab0ae9be82607e257f36a9
faisal-fida <arainfaisal826@gmail.com>
2023-09-24 09:39:48 +0500

    req

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main_page():
-    text_string = request.get_json()
-    return jsonify(text_string)
+    try:
+        text_string = request.get_json()
+        return jsonify(text_string)
+
+    except Exception as e:
+        response_error = {'Error':e}
+        return response_error, 500
+
+app.run()
